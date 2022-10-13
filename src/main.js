f5190649d5a074a179c96c782ce4444dc5ba0093
Wilber Ballesteros <ballestas01@gmail.com>
2022-10-13 11:55:25 -0500

    se elimina codigo hardcodeado de html y se agrega la funcion asincrona para listar las categorias

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ async function getTrendingMoviesPreview() {
     //console.log({data, movies});
     movies.forEach(movie => {
 
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
@@ -23,4 +23,33 @@ async function getTrendingMoviesPreview() {
 
 }
 
+async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres; //genres por q asi esta en la api
+    //console.log({data, movies});
+    categories.forEach(category => {
+
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+       
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);//el 2 id trae los colores
+        const categoryTitleText = document.createTextNode(category.name);
+   
+    //poneerlo en el html
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+
+    });
+
+}
+
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
